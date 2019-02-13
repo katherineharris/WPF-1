1c34cc052a9ab0e85ea8ec99ffcdab7026c81fa8
harr1626 <harr1626@PCB-LAB360-24>
2019-02-13 11:13:34 -0600

    class stuff XD

---
+++ WPF-1/WPF-Files/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Files
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            var result = dlg.ShowDialog();
+
+            txtFilePath.Text = dlg.FileName;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnReadFile_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(txtFilePath.Text) == true)
+            {
+                var lines = File.ReadAllLines(txtFilePath.Text);
+                var columnNames = lines[0].Split(',');
+                foreach(var line in lines)
+                {
+                    txtOutput.Text += line + "\n";
+                }
+            }
+        }
+    }
+}
